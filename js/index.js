1036df7ef869bdf519bcbbc1be52a595bd9ff814
Peter Kreppein <pmkreppein@gmail.com>
2019-01-11 06:16:40 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
